03385ed89ba7599cccf2b57e9e88c07e2e8c08ba
AhmedAlDiab <ahmed@AHMEDALDIABPC>
2025-03-09 22:23:41 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Todolist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AAD Sends Regards");
+            Console.ReadLine();
+        }
+    }
+}

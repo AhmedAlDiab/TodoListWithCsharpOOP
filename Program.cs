ed0b64f5a10c1ef7c5910589ac9109cabaec9978
Ahmed Abdulkarim <ahmedaldiabdev@gmail.com>
2025-03-09 22:37:41 +0200

    To Do later

--- Program.cs
+++ Program.cs
@@ -2,8 +2,23 @@
 
 namespace Todolist
 {
+    class ToDoTask
+    {
+        int Id { get; set; }
+        string Title { get; set; }
+        string Description { get; set; }
+        bool IsCompleted { get; set; }
+        public ToDoTask (int id, string title, string description)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            IsCompleted = false;
+        }
+    }
     class Program
     {
+        //to-do a list to store tasks and return them and methods to do so (maybe i will make another class for management (IDK --\(-_-)/--))
         static void Main(string[] args)
         {
             Console.WriteLine("AAD Sends Regards");

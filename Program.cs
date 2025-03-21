21b9c68d0944c123ab156bddd66813e573c61481
Ahmed Abdulkarim <ahmedaldiabdev@gmail.com>
2025-03-21 22:38:40 +0200

    Saving

--- Program.cs
+++ Program.cs
@@ -3,26 +3,45 @@
 namespace Todolist
 {
     class ToDoTask
-    {
-        int Id { get; set; }
-        string Title { get; set; }
-        string Description { get; set; }
-        bool IsCompleted { get; set; }
-        public ToDoTask (int id, string title, string description)
-        {
-            Id = id;
+    {        
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public ToDoTask (string title, string description)
+        {            
             Title = title;
             Description = description;
             IsCompleted = false;
         }
     }
     class Program
-    {
-        //to-do a list to store tasks and return them and methods to do so (maybe i will make another class for management (IDK --\(-_-)/--))
+    {        
+        List<ToDoTask> tasks = new List<ToDoTask>();
         static void Main(string[] args)
         {
-            Console.WriteLine("AAD Sends Regards");
-            Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine("1: Display Lists \n2: Make a new Task\n3:delete a task");
+                int choice;
+                while (!int.TryParse(Console.ReadLine(),out choice) || choice > 3 || choice < 1)
+                {
+                    Console.WriteLine("Invalid choice, please try again");
+                }
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Displaying Lists");
+                        break;
+                    case 2:
+                        Console.WriteLine("Make a new Task");
+                        break;
+                    case 3:
+                        Console.WriteLine("delete a task");
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
